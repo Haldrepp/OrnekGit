0edd20bb38b0a400ce80eed6de664aadf75eb46b
Alper Karaman <alperkaramann@hotmail.com>
2022-07-17 19:23:54 +0300

    1Kayıt

---
+++ OrnekGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OrnekGit
+//
+//  Created by Alper Karaman on 17.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("1.Kayıt")
+    }
+
+
+}
+
